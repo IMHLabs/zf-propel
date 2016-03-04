563b3a96babdbb7161301622842d4d63e7ecbba3
Mike Hughes <mikehu@inmotionhosting.com>
2016-03-04 14:17:56 -0800

    Prevent migrations if not configured'

--- src/ImhPropel/Propel/Adapter/PropelV2.php
+++ src/ImhPropel/Propel/Adapter/PropelV2.php
@@ -91,56 +91,60 @@ class PropelV2 extends Adapter
     public function diff()
 	{
 	    $configCreated = $this->createConfig();
-	    if ($configCreated) {
-	        $excludedTables = array();
-	        $modulemanager  = $this->getServiceLocator()->get('ModuleManager');
-	        $moduleObj 		= $modulemanager->loadModule($this->getModuleName());
-            if (method_exists ( $moduleObj, 'getConfig')) {
-    	        $module_config 	= $moduleObj->getConfig();
-    	        $connections = array_keys($module_config['propel']['database']['connections']);
-    	        //Get List of Modules pointing at database of current Module
-    	        $modules = $modulemanager->getModules();
-    	        foreach ($modules as $module) {
-    	            if ($module != $this->getModuleName()) {
-    	                $moduleObj 		= $modulemanager->loadModule($module);
-                        if (method_exists ( $moduleObj, 'getConfig')) {
-        	                $module_config 	= $moduleObj->getConfig();
-        	                if (isset($module_config['propel'])) {
-        	                    $module_connections = array_keys($module_config['propel']['database']['connections']);
-        	                    if (array_intersect($connections, $module_connections)) {
-        	                        // Get Module Schema Path
-        	                        $module_schema_path      = realpath($module_config['propel']['paths']['schema']);
-        	                        //Used same connection, Read schema
-        	                        $xml = (array) simplexml_load_file($module_schema_path . '/schema.xml');
-        	                        foreach ($xml['table'] as $table) {
-        	                            $table = (array) $table;
-        	                            $attributes = $table['@attributes'];
-        	                            $excludedTables[] = $attributes['name'];
-        	                        }
-        	                    }
-        	                }
-                        }
-    	            }
-    	        }
-    	        $propel_command			= sprintf(
-    	                '%s/bin/propel migration:diff --config-dir="%s" --schema-dir="%s" --output-dir="%s"',
-    	                $this->_getVendorPath(),
-    	                $this->getModuleConfigPath(),
-    	                $this->getModuleSchemaPath(),
-    	                $this->getModuleMigrationPath());
-    	        foreach ($excludedTables as $table) {
-    	            $propel_command .= ' --skip-tables="' . $table . '"';
-    	        }
-    	        $results = exec($propel_command,$output);
-    	        print "Building Migrations Files for Module: " . $this->getModuleName() . "\n";
-    	        print implode("\n",$output);
-    	        if (preg_match('/no diff to generate/',$results)) {
-    	            return false;
-    	        }
-            }
-	        return true;
-	    }
-	}
+        if ($this->getModuleMigrationPath()) {
+    	    if ($configCreated) {
+    	        $excludedTables = array();
+    	        $modulemanager  = $this->getServiceLocator()->get('ModuleManager');
+    	        $moduleObj 		= $modulemanager->loadModule($this->getModuleName());
+                if (method_exists ( $moduleObj, 'getConfig')) {
+        	        $module_config 	= $moduleObj->getConfig();
+        	        $connections = array_keys($module_config['propel']['database']['connections']);
+        	        //Get List of Modules pointing at database of current Module
+        	        $modules = $modulemanager->getModules();
+        	        foreach ($modules as $module) {
+        	            if ($module != $this->getModuleName()) {
+        	                $moduleObj 		= $modulemanager->loadModule($module);
+                            if (method_exists ( $moduleObj, 'getConfig')) {
+            	                $module_config 	= $moduleObj->getConfig();
+            	                if (isset($module_config['propel'])) {
+            	                    $module_connections = array_keys($module_config['propel']['database']['connections']);
+            	                    if (array_intersect($connections, $module_connections)) {
+            	                        // Get Module Schema Path
+            	                        $module_schema_path      = realpath($module_config['propel']['paths']['schema']);
+            	                        //Used same connection, Read schema
+            	                        $xml = (array) simplexml_load_file($module_schema_path . '/schema.xml');
+            	                        foreach ($xml['table'] as $table) {
+            	                            $table = (array) $table;
+            	                            $attributes = $table['@attributes'];
+            	                            $excludedTables[] = $attributes['name'];
+            	                        }
+            	                    }
+            	                }
+                            }
+        	            }
+        	        }
+        	        $propel_command			= sprintf(
+        	                '%s/bin/propel migration:diff --config-dir="%s" --schema-dir="%s" --output-dir="%s"',
+        	                $this->_getVendorPath(),
+        	                $this->getModuleConfigPath(),
+        	                $this->getModuleSchemaPath(),
+        	                $this->getModuleMigrationPath());
+        	        foreach ($excludedTables as $table) {
+        	            $propel_command .= ' --skip-tables="' . $table . '"';
+        	        }
+        	        $results = exec($propel_command,$output);
+        	        print "Building Migrations Files for Module: " . $this->getModuleName() . "\n";
+        	        print implode("\n",$output);
+        	        if (preg_match('/no diff to generate/',$results)) {
+        	            return false;
+        	        }
+                }
+    	        return true;
+    	    }
+        } else {
+	       print "Migrations not supported for Module: " . $this->getModuleName() . "\n";
+        }
+    }
     
     /**
 	 * Generate Empty Migration File for data migration
@@ -149,28 +153,32 @@ class PropelV2 extends Adapter
 	 */
     public function dataMigration()
 	{
-        print "\nBuilding Data Migration File for Module: " . $this->getModuleName() . "\n";
-        $manager = new MigrationManager();
-        $timestamp = time();
-        $migrationFileName  = $manager->getMigrationFileName($timestamp);
-        $migrationQuery = array(
-            $this->getModuleDbConnection() => '
-                # This is a fix for InnoDB in MySQL >= 4.1.x
-                # It "suspends judgement" for fkey relationships until are tables are set.
-                SET FOREIGN_KEY_CHECKS = 0;
-
-                # This restores the fkey checks, after having unset them earlier
-                SET FOREIGN_KEY_CHECKS = 1;
-                ',
-         );
-        $migrationClassBody = $manager->getMigrationClassBody($migrationQuery, $migrationQuery, $timestamp, null,  $this->getModuleName());
-        $migration_path         = $this->getModuleMigrationPath();
-        $filename = $migration_path . '/' . $migrationFileName;
-        //Generate Data Migration File Here
-        file_put_contents($filename, $migrationClassBody); 
-        print "\n" . realpath($filename) . " file successfully created for data migration\n";
-        print "Once the migration class is valid, call the \"migrate\" task to execute it.\n";
-	}
+        if ($this->getModuleMigrationPath()) {
+    	    print "\nBuilding Data Migration File for Module: " . $this->getModuleName() . "\n";
+            $manager = new MigrationManager();
+            $timestamp = time();
+            $migrationFileName  = $manager->getMigrationFileName($timestamp);
+            $migrationQuery = array(
+                $this->getModuleDbConnection() => '
+                    # This is a fix for InnoDB in MySQL >= 4.1.x
+                    # It "suspends judgement" for fkey relationships until are tables are set.
+                    SET FOREIGN_KEY_CHECKS = 0;
+    
+                    # This restores the fkey checks, after having unset them earlier
+                    SET FOREIGN_KEY_CHECKS = 1;
+                    ',
+             );
+            $migrationClassBody = $manager->getMigrationClassBody($migrationQuery, $migrationQuery, $timestamp, null,  $this->getModuleName());
+            $migration_path         = $this->getModuleMigrationPath();
+            $filename = $migration_path . '/' . $migrationFileName;
+            //Generate Data Migration File Here
+            file_put_contents($filename, $migrationClassBody); 
+            print "\n" . realpath($filename) . " file successfully created for data migration\n";
+            print "Once the migration class is valid, call the \"migrate\" task to execute it.\n";
+        } else {
+	       print "Migrations not supported for Module: " . $this->getModuleName() . "\n";
+        }
+    }
 
     /**
 	 * Execute migrations down
@@ -180,17 +188,21 @@ class PropelV2 extends Adapter
     public function down()
 	{
         $configCreated = $this->createConfig();
-        if ($configCreated) {
-			$propel_command			= sprintf(
-    	        '%s/bin/propel migration:down --config-dir="%s" --output-dir="%s"',
-			    $this->_getVendorPath(),
-			    $this->getModuleConfigPath(),
-				$this->getModuleMigrationPath());
-			$results = exec($propel_command,$output);
-            print "Executing Propel Down for Module: " . $this->getModuleName() . "\n";
-            print implode("\n",$output);
-		}
-	}
+        if ($this->getModuleMigrationPath()) {
+            if ($configCreated) {
+    			$propel_command			= sprintf(
+        	        '%s/bin/propel migration:down --config-dir="%s" --output-dir="%s"',
+    			    $this->_getVendorPath(),
+    			    $this->getModuleConfigPath(),
+    				$this->getModuleMigrationPath());
+    			$results = exec($propel_command,$output);
+                print "Executing Propel Down for Module: " . $this->getModuleName() . "\n";
+                print implode("\n",$output);
+    		}
+        } else {
+        	print "Migrations not supported for Module: " . $this->getModuleName() . "\n";
+        }
+    }
     
     /**
 	 * Execute all pending migrations
@@ -200,16 +212,20 @@ class PropelV2 extends Adapter
     public function migrate()
 	{
         $configCreated = $this->createConfig();
-        if ($configCreated) {
-			$propel_command			= sprintf(
-				'%s/bin/propel migrate --config-dir="%s" --output-dir="%s"',
-			    $this->_getVendorPath(),
-			    $this->getModuleConfigPath(),
-				$this->getModuleMigrationPath());
-			$results = exec($propel_command,$output);
-            print "Executing Propel Migrations for Module: " . $this->getModuleName() . "\n";
-            print implode("\n",$output);
-		}
+        if ($this->getModuleMigrationPath()) {
+            if ($configCreated) {
+    			$propel_command			= sprintf(
+    				'%s/bin/propel migrate --config-dir="%s" --output-dir="%s"',
+    			    $this->_getVendorPath(),
+    			    $this->getModuleConfigPath(),
+    				$this->getModuleMigrationPath());
+    			$results = exec($propel_command,$output);
+                print "Executing Propel Migrations for Module: " . $this->getModuleName() . "\n";
+                print implode("\n",$output);
+    		}
+        } else {
+            print "Migrations not supported for Module: " . $this->getModuleName() . "\n";
+        }
 	}
     
     /**
@@ -219,19 +235,23 @@ class PropelV2 extends Adapter
 	 */
     public function status()
 	{
-	        $configCreated = $this->createConfig();
-        if ($configCreated) {
-			$propel_command			= sprintf(
-    	        '%s/bin/propel migration:status --verbose --config-dir="%s" --output-dir="%s"',
-			    $this->_getVendorPath(),
-			    $this->getModuleConfigPath(),
-				$this->getModuleMigrationPath());
-			$results = exec($propel_command,$output);
-            print "Executing Propel Status for Module: " . $this->getModuleName() . "\n";
-            print implode("\n",$output);
-            return $output;
-		}
-	}
+	    $configCreated = $this->createConfig();
+        if ($this->getModuleMigrationPath()) {
+    	    if ($configCreated) {
+    			$propel_command			= sprintf(
+        	        '%s/bin/propel migration:status --verbose --config-dir="%s" --output-dir="%s"',
+    			    $this->_getVendorPath(),
+    			    $this->getModuleConfigPath(),
+    				$this->getModuleMigrationPath());
+    			$results = exec($propel_command,$output);
+                print "Executing Propel Status for Module: " . $this->getModuleName() . "\n";
+                print implode("\n",$output);
+                return $output;
+    		}
+        } else {
+	       print "Migrations not supported for Module: " . $this->getModuleName() . "\n";
+        }
+    }
 
     /**
 	 * Execute migrations up
@@ -241,15 +261,19 @@ class PropelV2 extends Adapter
     public function up()
 	{
         $configCreated = $this->createConfig();
-        if ($configCreated) {
-			$propel_command			= sprintf(
-    	        '%s/bin/propel migration:up --config-dir="%s" --output-dir="%s"',
-			    $this->_getVendorPath(),
-			    $this->getModuleConfigPath(),
-				$this->getModuleMigrationPath());
-			$results = exec($propel_command,$output);
-            print "Executing Propel Up for Module: " . $this->getModuleName() . "\n";
-            print implode("\n",$output);
-		}
-	}
+        if ($this->getModuleMigrationPath()) {
+            if ($configCreated) {
+    			$propel_command			= sprintf(
+        	        '%s/bin/propel migration:up --config-dir="%s" --output-dir="%s"',
+    			    $this->_getVendorPath(),
+    			    $this->getModuleConfigPath(),
+    				$this->getModuleMigrationPath());
+    			$results = exec($propel_command,$output);
+                print "Executing Propel Up for Module: " . $this->getModuleName() . "\n";
+                print implode("\n",$output);
+    		}
+        } else {
+	       print "Migrations not supported for Module: " . $this->getModuleName() . "\n";
+        }
+    }
 }
